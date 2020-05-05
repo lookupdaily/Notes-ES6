737b49727ad17514eedd45c422e72657d222b40e
Liz Daly <liz7daly@gmail.com>
2020-05-05 14:52:32 +0100

    Test for new note with text getter
    First test for note model.

---
+++ spec/note-model.spec.js
@@ -0,0 +1,11 @@
+describe('Note model', () => {
+  let note;
+
+  beforeEach(() => {
+    note = new Note('My favourite language is JavaScript');
+  });
+
+  it('creates a note with text', () => {
+    expect(note.text).toEqual('My favourite language is JavaScript')
+  });
+});
