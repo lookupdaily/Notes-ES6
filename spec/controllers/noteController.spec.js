e643d0afbeae6f399f94a4482d8abc2558b81910
Liz Daly <liz7daly@gmail.com>
2020-06-24 14:09:17 +0100

    Mock NoteListView return value in controller

--- spec/controllers/noteController.spec.js
+++ spec/controllers/noteController.spec.js
@@ -1,39 +1,54 @@
 import NoteList from '../../src/modules/noteList';
 import NoteController from '../../src/controllers/noteController';
 import NoteListView from '../../src/views/noteListView';
+import { JSDOM } from 'jsdom';
 
-jest.mock('../../src/modules/noteList')
-jest.mock('../../src/views/noteListView')
+jest.mock('../../src/modules/noteList');
+jest.mock('../../src/views/noteListView');
 
 describe('note controller', () => {
 
-  beforeEach(() => NoteList.mockClear())
-
-  it('stores a given note list as a parameter', () => {
-    const noteController = (new NoteController(new NoteList))
-    expect(noteController.noteList).toBeInstanceOf(NoteList)
-  })
+  beforeEach(() => {
+    NoteList.mockClear()
+    NoteListView.mockClear()
+  });
 
   it('creates a noteListView with note list', () => {
-    const noteList = new NoteList
-    new NoteController(noteList, NoteListView)
-    expect(NoteListView).toHaveBeenCalledWith(noteList)
-    
-  })
+    const noteList = new NoteList;
+    new NoteController(noteList);
+    expect(NoteListView).toHaveBeenCalledWith(noteList);
+  });
 
-  it('can add a new note to a note list', () => {
-    const noteList = new NoteList()
-    const noteController = (new NoteController(noteList))
-    noteController.addNote('new note')
-    expect(noteList.createNote).toHaveBeenCalledWith('new note')
-  })
+  it('can add a new note to a given note list', () => {
+    const noteList = new NoteList();
+    const noteController = (new NoteController(noteList));
+    noteController.addNote('my first note');
+    expect(noteList.createNote).toHaveBeenCalledWith('my first note');
+  });
 
   it('adding a note re-renders all notes', () => {
     // TODO
-  })
+  });
   
 
   it('inserts stored notes as HTML', () => {
-    //TODO
+    const document = new JSDOM(`<!DOCTYPE html><div id="notes">Hello, world!</div>`).window.document;
+    NoteList.mockImplementation(() => {
+      return {
+        notes: [{text: "my first note"}]
+      };
+    });
+    jest.mock('../../src/views/noteListView', () => {
+      return jest.fn().mockImplementation(() => {
+        return { notesToHTML: jest.fn.mockImplementation(() => {
+          return '<ul><li><div>my first note</div></li></ul>'
+        })} 
+      })
+    })
+
+    const noteController = (new NoteController(new NoteList));
+    console.log(document.getElementById('notes').innerHTML)
+    noteController.renderNotes();
+    expect(document.getElementById('notes')).toEqual('<ul><li><div>my first note</div></li></ul>');
   })
 })
--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -1,13 +1,19 @@
 import NoteListView from './../views/noteListView.js'
 
 class NoteController {
-  constructor(noteList, NoteListViewModule = NoteListView) {
+  constructor(noteList) {
     this.noteList = noteList
-    this.noteListView = new NoteListViewModule(this.noteList)
+    this.noteListView = new NoteListView(this.noteList)
   }
 
   addNote(text) {
     this.noteList.createNote(text)
+  }
+
+  renderNotes() {
+    console.log(this.noteListView.notesToHTMl())
+    console.log(document.getElementById('notes'))
+    document.getElementById('notes').innerHTML = this.noteListView.notesToHTMl()
   }
 }
 
